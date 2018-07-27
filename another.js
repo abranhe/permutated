6bd8eb72351756cab1a296a69959c6f10321b936
Carlos Abraham <abraham@abranhe.com>
2018-07-27 02:43:46 -0400

    remove local testing file

--- another.js
+++
@@ -1,9 +0,0 @@
-const permutated = require('./index.js');
-
-(async () => {
-	console.log(await permutated('abc'));
-	//=> [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-
-	console.log(await permutated.getLength('1234567'));
-	//=> 5040
-})();
